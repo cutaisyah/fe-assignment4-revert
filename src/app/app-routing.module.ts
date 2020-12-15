18b060f021d479225dca4099b5850700036bc80f
Louis Yupiter Consolatio <consolatio123@gmail.com>
2020-12-15 08:55:45 +0700

    push app-routing module doubled import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,3 @@
-
-import { LoginComponent } from './auth/login/login.component';
-
 import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './auth/login/login.component';
 import { NgModule } from '@angular/core';
