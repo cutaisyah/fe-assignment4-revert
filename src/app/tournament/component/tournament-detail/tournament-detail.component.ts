5922c940e285075512b86dcc4e78b394ca43b8e5
Louis Yupiter Consolatio <consolatio123@gmail.com>
2021-01-04 00:45:35 +0700

    feature/tournament, convert text to html

--- src/app/tournament/component/tournament-detail/tournament-detail.component.ts
+++ src/app/tournament/component/tournament-detail/tournament-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { TokenService } from 'src/app/services/token.service';
 import { UserService } from 'src/app/services/user.service';
@@ -16,12 +17,13 @@ export class TournamentDetailComponent implements OnInit {
   tournamentData: any;
   matches: any;
   teamMatch: any;
+  htmldescription: any;
 
   teams: [];
 
   public permalink: string;
 
-  constructor(public route: ActivatedRoute, public userService: UserService, public tokenService: TokenService) { }
+  constructor(public route: ActivatedRoute, public userService: UserService, public tokenService: TokenService, private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
 
@@ -29,65 +31,73 @@ export class TournamentDetailComponent implements OnInit {
       this.permalink = paramMap.get('permalink');
       this.userService.getDetailTournament(this.permalink).subscribe(data => {
         this.tournamentData = data;
+        this.htmldescription = this.sanitizer.bypassSecurityTrustHtml(this.tournamentData.description);
+        // console.log(this.htmldescription)
       });
       this.tournamentId = paramMap.get('tournamentId');
       this.userService.getTheMatchScore(this.tournamentId).subscribe(matchdata => {
         this.matches = matchdata.match;
+        // console.log(this.matches);
 
-        // let minimalData = {
-        //   teams: [],
-        //   results: []
-        // };
-        
-        // this.teamMatch = this.matches.filter(match => match.match_round == 1);
-        // const teamname = this.teamMatch.map(o => o.team.team_name)
-        // while (teamname.length > 0) {
-        //   if(teamname.length % 2 != 0){
-        //     teamname.push(null)
-        //   }else{
-        //     let teamTemp = teamname.splice(0, 2)
-        //     minimalData.teams.push(teamTemp)
-        //   }
-        // }
-
-        // let array_score = [];
-        // var index = 0;
-        // let temp_array = [];
-
-        // for(const i in this.matches){
-        //   if(index != this.matches[i].match_round){
-        //     index = this.matches[i].match_round;
-        //     array_score[index-1] = [];
-        //     temp_array = [];
-        //   }
-        //   temp_array.push(this.matches[i].score);
-        //   if(temp_array.length > 1){
-        //     array_score[index-1].push(temp_array);
-        //     temp_array = [];
-        //   }
-        // }
-
-        // minimalData.results.push(array_score)
-
-        var minimalData = {
-          teams : [
-                ["CAL", "Team4"],
-                ["Team5", "Team6"],
-              ["TEAM7", "TEAM8"],
-              ["TEAM9", null]
-              ],
-          results: [
-                      [[100,50],[32,30],[89,95],[23]],
-                      [[50,42],[84,100]],
-                     [[100,60],[60,80]]
-                    ]
+        if(this.tournamentData?.categories =="single elimination"){
+          let minimalData = {
+            teams: [],
+            results: []
+          };
+          
+          this.teamMatch = this.matches?.filter(match => match.match_round == 1);
+          const teamname = this.teamMatch?.map(o => o.team.team_name)
+          while (teamname?.length > 0) {
+            if(teamname.length % 2 != 0){
+              teamname.push(null)
+            }else{
+              let teamTemp = teamname.splice(0, 2)
+              minimalData.teams.push(teamTemp)
+            }
           }
-
-        $(document).ready(() => {
-          $('#minimal .demo').bracket({
-            init: minimalData
+  
+          let array_score = [];
+          var index = 0;
+          let temp_array = [];
+  
+          for(const i in this.matches){
+            if(index != this.matches[i].match_round){
+              index = this.matches[i].match_round;
+              array_score[index-1] = [];
+              temp_array = [];
+            }
+            temp_array.push(this.matches[i].score);
+            if(temp_array.length > 1){
+              array_score[index-1].push(temp_array);
+              temp_array = [];
+            }
+          }
+  
+          minimalData.results.push(array_score)
+  
+          // var minimalData = {
+          //   teams : [
+          //         ["CAL", "Team4"],
+          //         ["Team5", "Team6"],
+          //       ["TEAM7", "TEAM8"],
+          //       ["TEAM9", null]
+          //       ],
+          //   results: [
+          //               [[100,50],[32,30],[89,95],[23]],
+          //               [[50,42],[84,100]],
+          //              [[100,60],[60,80]]
+          //             ]
+          //   }
+  
+          $(document).ready(() => {
+            $('#minimal .demo').bracket({
+              init: minimalData
+            })
           })
-        })
+        } else{
+          return;
+        }
+        
 
       });
     });
@@ -97,7 +107,7 @@ export class TournamentDetailComponent implements OnInit {
   }
 
   register() {
-    this.userService.pesertaRegisterTournament(this.permalink);
+    this.userService.pesertaRegisterTournament(this.tournamentId, this.permalink);
   }
 
 
